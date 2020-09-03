628f84135fb981863ce3ad5fbd4ebc32e8746ae4
Merwane Draï <merwanedr@gmail.com>
2020-09-03 17:56:03 +0200

    add opcodes

--- oogway/__init__.py
+++ oogway/__init__.py
@@ -6,6 +6,7 @@ from .fees import get_fees
 from .request import request_payment, parse_request
 from .net import Net
 from .rpc import AuthServiceProxy as Node
+from .utils.opcodes import script
 
 name = "oogway"
 __version__ = "0.5.0"
